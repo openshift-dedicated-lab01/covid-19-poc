a83c6dae5c486324196bafd42f6e5dac71d743b4
naultse <naultse@Z500L22370.le500.loto-quebec.com>
2020-03-20 14:37:38 -0400

    Adding empty tracker proxy to repo.

---
+++ TrackerProxy/src/main/java/com/covid/TrackerProxy/TrackerProxyApplication.java
@@ -0,0 +1,13 @@
+package com.covid.TrackerProxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackerProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackerProxyApplication.class, args);
+	}
+
+}
---
+++ TrackerProxy/src/test/java/com/covid/TrackerProxy/TrackerProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.TrackerProxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackerProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
