f32439b7429822415d067203059c094203836389
naultse <naultse@Z500L22370.le500.loto-quebec.com>
2020-03-20 16:03:49 -0400

    TrackerStats object for json feeding.

--- TrackerProxy/src/main/java/com/covid/TrackerProxy/TrackerProxyApplication.java
+++ TrackerProxy/src/main/java/com/covid/TrackerProxy/TrackerProxyApplication.java
@@ -1,13 +1,34 @@
 package com.covid.TrackerProxy;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class TrackerProxyApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(TrackerProxyApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TrackerProxyApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			TrackerStats trackerStats = restTemplate.getForObject(
+					"https://corona-stats.online/Canada?format=json", TrackerStats.class);
+			log.info(Integer.toString(trackerStats.getTrackerStats().size()));
+		};
+	}
 }
