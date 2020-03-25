5b8641105db8399b088cb93a623143bb18ee5d2a
naultse <naultse@Z500L22370.le500.loto-quebec.com>
2020-03-25 09:57:51 -0400

    SQL pour créer la BD

--- rest/src/main/resources/data.sql
+++ rest/src/main/resources/data.sql
@@ -1,63 +1,68 @@
-SET foreign_key_checks = 0;
-DROP TABLE IF EXISTS confirmedByDay;
-DROP TABLE IF EXISTS recoveredByDay;
-DROP TABLE IF EXISTS deathsByDay;
-DROP TABLE IF EXISTS statistic;
-SET foreign_key_checks = 1;
+--SET foreign_key_checks = 0;
+--ALTER TABLE confirmed_by_day DROP FOREIGN KEY statistic_id_fk;
+--DROP TABLE IF EXISTS confirmed_by_day;
+--ALTER TABLE recovered_by_day 
+--	DROP FOREIGN KEY statistic_id_fk;
+--DROP TABLE IF EXISTS recovered_by_day;
+--ALTER TABLE deaths_by_day 
+--	DROP FOREIGN KEY statistic_id_fk;
+--DROP TABLE IF EXISTS deaths_by_day;
+--DROP TABLE IF EXISTS statistic;
+--SET foreign_key_checks = 1;
  
  --https://corona-stats.online/?format=json
  
-CREATE TABLE statistic (
-  statisticId BIGINT NOT NULL AUTO_INCREMENT,
-  ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
-  country VARCHAR(50) DEFAULT NULL,
-  province VARCHAR(50) DEFAULT NULL,
-  country_code VARCHAR(2) DEFAULT NULL,
-  confirmed VARCHAR(20) DEFAULT NULL,
-  recovered VARCHAR(20) DEFAULT NULL,
-  deaths VARCHAR(20) DEFAULT NULL,
-  confirmedByDay INT,
-  recoveredByDay INT,
-  deathsByDay INT,
-  last_updated VARCHAR(50) DEFAULT NULL,
-  active VARCHAR(20) DEFAULT NULL,
-  mortality_per VARCHAR(20) DEFAULT NULL,
-  recovered_per VARCHAR(20) DEFAULT NULL,
-  CONSTRAINT statistic_pk PRIMARY KEY (statisticId)
-);
+-- CREATE TABLE statistic (
+--   statistic_id BIGINT NOT NULL AUTO_INCREMENT,
+--   ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
+--   country VARCHAR(50) DEFAULT NULL,
+--   province VARCHAR(50) DEFAULT NULL,
+--   country_code VARCHAR(2) DEFAULT NULL,
+--   confirmed VARCHAR(20) DEFAULT NULL,
+--   recovered VARCHAR(20) DEFAULT NULL,
+--   deaths VARCHAR(20) DEFAULT NULL,
+--   confirmed_by_day INT,
+--   recovered_by_day INT,
+--   deaths_by_day INT,
+--   last_updated VARCHAR(50) DEFAULT NULL,
+--   active VARCHAR(20) DEFAULT NULL,
+--   mortality_per VARCHAR(20) DEFAULT NULL,
+--   recovered_per VARCHAR(20) DEFAULT NULL,
+--   CONSTRAINT statistic_pk PRIMARY KEY (statistic_id)
+-- );
+-- 
+-- CREATE TABLE confirmed_by_day (
+--   confirmed_by_dayId BIGINT NOT NULL AUTO_INCREMENT,
+--   statistic_id BIGINT NOT NULL,
+--   confirmed_by_day INT,
+--   CONSTRAINT confirmed_by_day_pk PRIMARY KEY (confirmed_by_dayId),
+--   CONSTRAINT statistic_id_fk FOREIGN KEY (statistic_id)
+--         REFERENCES statistic(statistic_id)
+--         ON DELETE CASCADE
+-- );
+--   
+-- CREATE TABLE recovered_by_day (
+--   recovered_by_dayId BIGINT NOT NULL AUTO_INCREMENT,
+--   statistic_id BIGINT NOT NULL,
+--   recovered_by_day INT,
+--   CONSTRAINT recovered_by_day_pk PRIMARY KEY (recovered_by_dayId),
+--   CONSTRAINT statistic_id_fk FOREIGN KEY (statistic_id)
+--         REFERENCES statistic(statistic_id)
+--         ON DELETE CASCADE
+-- );
 
-CREATE TABLE confirmedByDay (
-  confirmedByDayId BIGINT NOT NULL AUTO_INCREMENT,
-  statisticId BIGINT NOT NULL,
-  confirmedByDay INT,
-  CONSTRAINT confirmedByDay_pk PRIMARY KEY (confirmedByDayId),
-  FOREIGN KEY (statisticId)
-        REFERENCES statistic(statisticId)
-        ON DELETE CASCADE
-);
-  
-CREATE TABLE recoveredByDay (
-  recoveredByDayId BIGINT NOT NULL AUTO_INCREMENT,
-  statisticId BIGINT NOT NULL,
-  recoveredByDay INT,
-  CONSTRAINT recoveredByDay_pk PRIMARY KEY (recoveredByDayId),
-  FOREIGN KEY (statisticId)
-        REFERENCES statistic(statisticId)
-        ON DELETE CASCADE
-);
-
-CREATE TABLE deathsByDay (
-  deathsByDayId BIGINT NOT NULL AUTO_INCREMENT,
-  statisticId BIGINT NOT NULL,
-  deathsByDay INT,
-  CONSTRAINT deathsByDay_pk PRIMARY KEY (deathsByDayId),
-  FOREIGN KEY (statisticId)
-        REFERENCES statistic(statisticId)
-        ON DELETE CASCADE
-);
+-- CREATE TABLE deaths_by_day (
+--   deaths_by_day_id BIGINT NOT NULL AUTO_INCREMENT,
+--   statistic_id BIGINT NOT NULL,
+--   deaths_by_day INT,
+--   CONSTRAINT deaths_by_day_pk PRIMARY KEY (deaths_by_dayId),
+--   CONSTRAINT statistic_id_fk FOREIGN KEY (statistic_id)
+--         REFERENCES statistic(statistic_id)
+--         ON DELETE CASCADE
+-- );
 
    
---INSERT INTO statistic (country, province, country_code, confirmed, recovered, deaths, confirmedByDay, recoveredByDay, deathsByDay, last_updated, active, mortality_per, recovered_per) VALUES
+--INSERT INTO statistic (country, province, country_code, confirmed, recovered, deaths, confirmed_by_day, recovered_by_day, deaths_by_day, last_updated, active, mortality_per, recovered_per) VALUES
 --  ('Canada', 'Québec', 'CA', '345','124','34',0,0,0,'2020-03-21','4','5','6');
 
 INSERT INTO statistic (country, province, country_code, confirmed, recovered, deaths, last_updated, active, mortality_per, recovered_per) VALUES
