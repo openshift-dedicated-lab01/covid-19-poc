2ebc1f2c311cbb0689e045bb789cff2a52b268bf
naultse <naultse@Z500L22370.le500.loto-quebec.com>
2020-03-25 08:58:43 -0400

    Merge branch 'develop' of git@github.com:openshift-dedicated-lab01/covid-19-poc.git into develop
    Conflicts:
    	rest/src/main/java/com/covid/entity/Statistics.java
    	rest/src/main/resources/data.sql

--- rest/src/main/resources/data.sql
+++ rest/src/main/resources/data.sql
@@ -1,12 +1,13 @@
 DROP TABLE IF EXISTS statistics;
+DROP TABLE IF EXISTS statistic;
 DROP TABLE IF EXISTS confirmedByDay;
 DROP TABLE IF EXISTS recoveredByDay;
 DROP TABLE IF EXISTS deathsByDay;
  
  --https://corona-stats.online/?format=json
  
-CREATE TABLE statistics (
-  statisticsId BIGINT NOT NULL AUTO_INCREMENT,
+CREATE TABLE statistic (
+  statisticId BIGINT NOT NULL AUTO_INCREMENT,
   ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, 
   country VARCHAR(50) DEFAULT NULL,
   province VARCHAR(50) DEFAULT NULL,
@@ -21,40 +22,40 @@ CREATE TABLE statistics (
   active VARCHAR(20) DEFAULT NULL,
   mortalityPer VARCHAR(20) DEFAULT NULL,
   recoveredPer VARCHAR(20) DEFAULT NULL,
-  CONSTRAINT statistics_pk PRIMARY KEY (statisticsId)
+  CONSTRAINT statistic_pk PRIMARY KEY (statisticId)
 );
 
 CREATE TABLE confirmedByDay (
   confirmedByDayId BIGINT NOT NULL AUTO_INCREMENT,
-  statisticsId BIGINT NOT NULL,
+  statisticId BIGINT NOT NULL,
   confirmedByDay BIGINT,
   CONSTRAINT confirmedByDay_pk PRIMARY KEY (confirmedByDayId),
-  FOREIGN KEY (statisticsId)
-        REFERENCES statistics(statisticsId)
+  FOREIGN KEY (statisticId)
+        REFERENCES statistic(statisticId)
         ON DELETE CASCADE
 );
   
 CREATE TABLE recoveredByDay (
   recoveredByDayId BIGINT NOT NULL AUTO_INCREMENT,
-  statisticsId BIGINT NOT NULL,
+  statisticId BIGINT NOT NULL,
   recoveredByDay INT,
   CONSTRAINT recoveredByDay_pk PRIMARY KEY (recoveredByDayId)
-  FOREIGN KEY (statisticsId)
-        REFERENCES statistics(statisticsId)
+  FOREIGN KEY (statisticId)
+        REFERENCES statistic(statisticId)
         ON DELETE CASCADE
 );
 
 CREATE TABLE deathsByDay (
   deathsByDayId BIGINT NOT NULL AUTO_INCREMENT,
-  statisticsId BIGINT NOT NULL,
+  statisticId BIGINT NOT NULL,
   deathsByDay INT,
   CONSTRAINT deathsByDay_pk PRIMARY KEY (deathsByDayId)
-  FOREIGN KEY (statisticsId)
-        REFERENCES statistics(statisticsId)
+  FOREIGN KEY (statisticId)
+        REFERENCES statistic(statisticId)
         ON DELETE CASCADE
 );
    
-INSERT INTO statistics (country, province, countryCode, confirmed, recovered, deaths, confirmedByDay, recoveredByDay, deathsByDay, lastUpdated, active, mortalityPer, recoveredPer) VALUES
+INSERT INTO statistic (country, province, countryCode, confirmed, recovered, deaths, confirmedByDay, recoveredByDay, deathsByDay, lastUpdated, active, mortalityPer, recoveredPer) VALUES
   ('Canada', 'Québec', 'CA', '345','124','34',0,0,0,'2020-03-21','4','5','6');
 
 INSERT INTO statistic (country, province, country_code, confirmed, recovered, deaths, last_updated, active, mortality_per, recovered_per) VALUES
